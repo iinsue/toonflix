a84ff64fca58307cd83b24c93d4628c74da64569
iinsue <hinsu93@gmail.com>
2025-04-20 22:30:31 +0900

    #4.0 State

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:toonflix/widgets/stateless_example.dart';
+// import 'package:toonflix/widgets/stateless_example.dart';
 
 void main() {
   runApp(const App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
 
   @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int counter = 0;
+
+  void onClicked() {
+    counter = counter + 1;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const StatelessExample();
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color(0xFFF4EDDB),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('Click Count', style: TextStyle(fontSize: 30)),
+              Text('$counter', style: const TextStyle(fontSize: 30)),
+              IconButton(
+                iconSize: 40,
+                onPressed: onClicked,
+                icon: const Icon(Icons.add_box_rounded),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
