fd4c8e4caf6fe33c11ee31abbde36af36dd9722d
insu <hinsu93@gmail.com>
2025-04-21 18:35:39 +0900

    #6.2 Data Fetching

--- lib/pomodoro/pomodoro.dart
+++ lib/pomodoro/pomodoro.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:toonflix/screens/home_screen.dart';
+import 'package:toonflix/pomodoro/home_screen.dart';
 
 class Pomodoro extends StatelessWidget {
   const Pomodoro({super.key});
